e8c925f4b1ab31d0c9cd955900f8e3e2f1f7b80d
Ahmad Rafi <rafisatulama@gmail.com>
2026-06-17 09:52:43 +0700

    security: upgrade encryption key in config.php to a secure random 32-character hex string

--- application/config/config.php
+++ application/config/config.php
@@ -326,7 +326,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/userguide3/libraries/encryption.html
 |
 */
-$config['encryption_key'] = 'peduli_mahasiswa_key_2026';
+$config['encryption_key'] = '6622a4391049e25558a86090af476ca4';
 
 /*
 |--------------------------------------------------------------------------
