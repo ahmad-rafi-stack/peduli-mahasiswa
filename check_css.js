e35e82a64830ec4b055ad5ed0c75e7602ce0cc16
Ahmad Rafi <rafisatulama@gmail.com>
2026-06-17 23:14:54 +0700

    clean: remove temporary test files

--- check_css.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-if (fs.existsSync('assets/css/tailwind.css')) {
-    const css = fs.readFileSync('assets/css/tailwind.css', 'utf8');
-    console.log('Contains translate-x-0:', css.includes('translate-x-0') || css.includes('.translate-x-0'));
-    console.log('Contains -translate-x-full:', css.includes('-translate-x-full') || css.includes('.-translate-x-full'));
-    console.log('Contains md:translate-x-0:', css.includes('md:translate-x-0') || css.includes('\\:translate-x-0'));
-    console.log('Contains ease-in-out:', css.includes('ease-in-out') || css.includes('.ease-in-out'));
-}
